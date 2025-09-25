b97c93511a965e2e44283f80336386519eeb1051
Voyager-001 <artyom.nicolay@hotmail.com>
2025-09-25 15:58:46 +0200

    Add DuelCard Project

---
+++ Personal Project/Treasure Hunt/Assets/Scripts/ClickRevealController.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickRevealController : MonoBehaviour
+{
+    public GridManager grid;
+    public LayerMask tileLayer = ~0;
+
+    void Update()
+    {
+        if (!GameManager.Instance || GameManager.Instance.state.Value != GameState.Playing) return;
+        if (!GameManager.Instance.IsMyTurn) return;
+
+        if (EventSystem.current && EventSystem.current.IsPointerOverGameObject()) return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            var cam = Camera.main;
+            if (!cam) return;
+
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 1000f, tileLayer))
+            {
+                var cell = hit.collider.GetComponent<TileCell>();
+                if (cell)
+                {
+                    var myId = NetworkManager.Singleton.LocalClientId;
+                    grid.TryRevealServerRpc(cell.GridPos, myId);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-1. Projet Mix&Shoot:
+- School Project
+
+Projet Mix&Shoot:\
 C'est un jeu produit durant un sprint d'une semaine, c'est un mix entre OverCooked et un Tower Defense.
 J'ai produit les contrôles des joueurs, la mécanique des vagues, la gestion des éléments pour les potions, la création des potions.
-
-2. TGCM:
+####
+Projet TGCM:\
 C'est un jeu produit pour mon examen de deuxième, on nous avait demander de faire un jeu en 2D en tour par tour, donc quand le joueur rencontre un enemie, il y a un combat de jeu de rythme comme Guitar Heroes.
 L'environnement est fait en tuile.
 
-3. Le petit exercice:
+- Personal Project
+
+Projet Treasure Hunt:\
 [Exercice _ Développement d'un jeu multijoueur - Grille et affrontement.pdf](https://github.com/user-attachments/files/22027160/Exercice._.Developpement.d.un.jeu.multijoueur.-.Grille.et.affrontement.pdf)
+####
+Projet DuelCard:\
+Un projet inspirer de Yu-Gi-Oh! Master Duel.
